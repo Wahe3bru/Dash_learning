5f6bdd920477c136c8de3000ba756ad4b58625af
Wahe3bru <wahe3bru@gmail.com>
2019-07-12 08:00:08 +0200

    added following and post pages with callbacks

--- 07-this_layout.py
+++ 07-this_layout.py
@@ -13,10 +13,13 @@ external_css = ["https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normaliz
                  "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
                 ]
 app = dash.Dash(__name__, external_stylesheets=external_css)
+# allows for callbacks that are not used immediately (ie on page load)
 app.config['suppress_callback_exceptions']=True
 
 ###---   Data Wrangling   ---###
-Follower_count_df = pd.read_csv('data/Follower_count_gsheet.csv')
+follower_count_df = pd.read_csv('data/Follower_count_gsheet.csv')
+following_count_df = pd.read_csv('data/Following_count_gsheet.csv')
+post_count_df = pd.read_csv('data/Post_count_gsheet.csv')
 
 def correct_num_data(x):
     """turn (1,234 and 11.7K) strings into integer values
@@ -32,8 +35,14 @@ def correct_num_data(x):
         y = y.replace(',', '').replace('k', '000')
     return int(y)
 
-Follower_count_df = Follower_count_df.set_index('Date')
-Follower_count_df = Follower_count_df.applymap(correct_num_data)
+follower_count_df = follower_count_df.set_index('Date')
+follower_count_df = follower_count_df.applymap(correct_num_data)
+
+following_count_df = following_count_df.set_index('Date')
+following_count_df = following_count_df.applymap(correct_num_data)
+
+post_count_df = post_count_df.set_index('Date')
+post_count_df = post_count_df.applymap(correct_num_data)
 
 # reusable dash_core_components
 def print_button():
@@ -139,7 +148,7 @@ followers = html.Div([
         # Row 4
         html.Div([
             html.H6(children='Follower Counts', className="gs-header gs-text-header padded"),
-            dcc.Tabs(id='tabs' ,value='tab-raw', children=[
+            dcc.Tabs(id='tabs-followers' ,value='tab-raw', children=[
                 dcc.Tab(label='Raw Numbers', value='tab-raw'),
                 dcc.Tab(label='Difference', value='tab-diff'),
                 dcc.Tab(label='Percentage Change', value='tab-perc'),
@@ -177,8 +186,31 @@ following = html.Div([
 
         # Row 3
         html.Div([
-            html.H4(children='Following')
-        ])
+            html.H6(children='Summary', className="gs-header gs-text-header padded"),
+            html.P(children= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sint aliquid dignissimos placeat iste vero atque iusto iure nulla dolore. Odio libero doloremque repellat neque quibusdam deserunt aut necessitatibus culpa."),
+            html.P(children= "Consectetur adipisicing elit. Quam sint aliquid dignissimos placeat iste vero atque iusto iure nulla dolore. Odio libero doloremque repellat neque quibusdam deserunt aut necessitatibus culpa."),
+        ], className="row ten columns"),
+
+        # Row 4
+        html.Div([
+            html.H6(children='Folowing Counts', className="gs-header gs-text-header padded"),
+            dcc.Tabs(id='tabs-following', value='tab-raw', children=[
+                dcc.Tab(label='Raw Numbers', value='tab-raw'),
+                dcc.Tab(label='Difference', value='tab-diff'),
+                dcc.Tab(label='Percentage Change', value='tab-perc'),
+            ], className="custom-tabs-container custom-tab custom-tabs custom-tab--selected "),
+            html.Div([
+                dcc.Graph(
+                    id='following_count_graph',
+                    style={'height':'450', 'width':'720'},
+                    config={
+                        'displayModeBar': False,
+                        'displaylogo': False
+                    }
+                )
+            ], className="twelve columns"),
+        ], className="row twelve columns")
+
     ], className="subpage")
 ], className="page")
 
@@ -197,8 +229,31 @@ posts = html.Div([
 
         # Row 3
         html.Div([
-            html.H4(children='Post')
-        ])
+            html.H6(children='Summary', className="gs-header gs-text-header padded"),
+            html.P(children= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sint aliquid dignissimos placeat iste vero atque iusto iure nulla dolore. Odio libero doloremque repellat neque quibusdam deserunt aut necessitatibus culpa."),
+            html.P(children= "Consectetur adipisicing elit. Quam sint aliquid dignissimos placeat iste vero atque iusto iure nulla dolore. Odio libero doloremque repellat neque quibusdam deserunt aut necessitatibus culpa."),
+        ], className="row ten columns"),
+
+        # Row 4
+        html.Div([
+            html.H6(children="Post Count", className="gs-header gs-text-header padded"),
+            dcc.Tabs(id='tabs-post', value='tab-raw', children=[
+                dcc.Tab(label='Raw Numbers', value='tab-raw'),
+                dcc.Tab(label='Difference', value='tab-diff'),
+                dcc.Tab(label='Percentage Change', value='tab-perc'),
+            ], className="custom-tabs-container custom-tab custom-tabs custom-tab--selected "),
+            html.Div([
+                dcc.Graph(
+                id='post_count_graph',
+                style={'height':'450', 'width':'720'},
+                config={
+                    'displayModeBar': False,
+                    'displaylogo': False
+                }
+            )
+            ], className="twelve columns"),
+        ], className="row twelve columns")
+
     ], className="subpage")
 ], className="page")
 
@@ -258,17 +313,17 @@ def display_page(pathname):
 
 @app.callback(
     Output('follower_count_graph', 'figure'),
-    [Input('tabs', 'value')])
+    [Input('tabs-followers', 'value')])
 def update_follower_count_graph(selected_data):
     ''' returns plot data based on selection
     '''
     # calc Data based on radio
     if selected_data == 'tab-diff':
-        df = Follower_count_df.diff()
+        df = follower_count_df.diff()
     elif selected_data == 'tab-perc':
-        df = Follower_count_df.pct_change()
+        df = follower_count_df.pct_change()
     else:
-        df = Follower_count_df
+        df = follower_count_df
 
     # prep data for return
     traces = []
@@ -288,6 +343,82 @@ def update_follower_count_graph(selected_data):
             height = 400,
             width = 725,
             # title='Follower Count',
+            xaxis = dict(
+                tickangle=45,
+            ),
+        )
+    }
+
+
+@app.callback(
+    Output('following_count_graph', 'figure'),
+    [Input('tabs-following', 'value')])
+def update_following_count_graph(selected_data):
+    ''' returns plot data based on selection
+    '''
+    # calc Data based on radio
+    if selected_data == 'tab-diff':
+        df = following_count_df.diff()
+    elif selected_data == 'tab-perc':
+        df = following_count_df.pct_change()
+    else:
+        df = following_count_df
+
+    # prep data for return
+    traces = []
+    for i in df.columns:
+        traces.append(
+            go.Scatter(
+                x=df.index,
+                y=df[i],
+                mode='markers+lines',
+                name=i,
+            )
+        )
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            autosize = True,
+            height = 400,
+            width = 725,
+            xaxis = dict(
+                tickangle=45,
+            ),
+        )
+    }
+
+
+@app.callback(
+    Output('post_count_graph', 'figure'),
+    [Input('tabs-post', 'value')])
+def update_post_graph(selected_data):
+    ''' returns plot data based on selection
+    '''
+    # calc Data based on radio
+    if selected_data == 'tab-diff':
+        df = post_count_df.diff()
+    elif selected_data == 'tab-perc':
+        df = post_count_df.pct_change()
+    else:
+        df = post_count_df
+
+    # prep data for return
+    traces = []
+    for i in df.columns:
+        traces.append(
+            go.Scatter(
+                x=df.index,
+                y=df[i],
+                mode='markers+lines',
+                name=i,
+            )
+        )
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            autosize = True,
+            height = 400,
+            width = 725,
             xaxis = dict(
                 tickangle=45,
             ),
